0b8f1590fc64de015614b100bc5550e495d945e6
dever <dever@gmail.com>
2023-05-18 14:41:46 +0000

    bug fix

--- client/src/components/auth/Login/Footer.tsx
+++ client/src/components/auth/Login/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
   return (
     <p className="text-center text-slate-500 text-sm">
-      Don't have an account
+      Don`&apos;`t have an account
       <br />
       <Link className="text-blue-500" href="/auth/register">
         Register
